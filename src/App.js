1ea448e3c48cacfee549915fbd220d8c69e2612b
lorenzoash <z.lakyo@gmail.com>
2018-06-29 02:01:11 -0700

    imported TimelineLite

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Power2, TimelineLite } from "gsap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.intro = React.createRef(); // shiny new React 16.3 ref API!
+    this.logo = React.createRef();
+    this.content = React.createRef();
+  }
+
+  componentDidMount() {
+    const tl = new TimelineLite();
+
+    tl.to(this.intro.current, 4.5, { opacity: 1, delay: 1 })
+      .to(this.intro.current, 1.5, { opacity: 0 })
+      .set(this.logo.current, { opacity: 1, scale: 2.75 })
+      .to(this.logo.current, 8, {
+        scale: 0.05,
+        ease: Power2.easeOut
+      })
+      .to(this.logo.current, 1.5, { opacity: 0 }, "-=1.5")
+      .to(this.content.current, 200, { top: "-170%"});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <section className="intro" ref={this.intro}>
+        
+          <p>
+            A long time ago, in a galaxy far, <br /> far away....
+          </p>
+        </section>
+        <section className="logo" ref={this.logo}>
+            <img src={logo} alt="Code Wars logo" />
+          </section>
+        <section className="crawl">
+            <div className="content" ref={this.content}>
+              <h1 className="title">Episode 7</h1>
+              <h2 className="subtitle">THE APP AWAKENS</h2>
+              <p>
+                The Development Team Lead has vanished. In her absence, the
+                sinister FUNCTIONAL BUG has risen from the ashes of the CI Tool
+                and will not rest until the last developer has been destroyed.
+              </p>
+              <p>
+                With the support of the QA TEAM, the Software Developer leads a
+                brave RESISTANCE. He is desperate to find his Lead and gain her
+                help in restoring peace and justice to the repository.
+              </p>
+              <p>
+                The Developer has sent his most daring editor theme on a secret
+                mission to the production branch, where an old ally has
+                discovered a clue to the Lead’s whereabouts....
+              </p>
+            </div>
+          </section>
       </div>
+      
     );
   }
 }
-
 export default App;
